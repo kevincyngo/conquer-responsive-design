cf6b9575b2113030115a9a6317120b898c54332c
Kevin Ngo <ngokevin39@gmail.com>
2019-09-11 00:15:10 -0400

    external linking jquery

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 $(".contact-form").submit(function(event) {
     alert("sent form.");
 });
+
+
+//allows external linking
+$(".navbar-nav li").click(function (event) {
+    if ($(this).hasClass("external")) {
+        event.preventDefault();
+    }
+});
